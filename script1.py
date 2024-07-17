a7758f13fb4e55fe0b1c27b0bfcbdf4e2494648c
raphael15002 <refols152345@gmail.com>
2024-07-17 13:05:55 -0300

    finalizado

--- script1.py
+++ script1.py
@@ -11,11 +11,19 @@ ano = []
 situacao = []
 objeto = []
 tipo_pessoa = []
-meus_links = []
 cpf_cnpj = []
 nome_fornecedor = []
 Razão_social = []
+meus_links = []
 
+def extract_text(soup, label_text):
+    label = soup.find(text=label_text)
+    if label:
+        sibling = label.find_next('p')
+        return sibling.text.strip() if sibling else None
+    return None
+
+#links dos contratos
 for i in range(0, 28):
     print(f"Página: {i + 1}\n")
 
@@ -43,32 +51,39 @@ for link in meus_links:
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             # Função para extrair texto com verificação de presença
-            def extract_text(soup, label_text):
-                label = soup.find(text=label_text)
-                if label:
-                    sibling = label.find_next('p')
-                    return sibling.text.strip() if sibling else None
-                return None
 
-            codigo_tce.append(extract_text(soup, "Código TCE:"))
-            n_instrumento.append(extract_text(soup, "Número Instrumento:"))
+            codigo_tce.append(extract_text(soup, "Código TCE: "))
+            n_instrumento.append(extract_text(soup, "Nº. Instrumento contratual:"))
             ano.append(extract_text(soup, "Ano:"))
             situacao.append(extract_text(soup, "Situação:"))
             objeto.append(extract_text(soup, "Objeto:"))
-            tipo_pessoa.append(extract_text(soup, "Tipo de Pessoa:"))
+            tipo_pessoa.append(extract_text(soup, "Tipo de pessoa:"))
             cpf_cnpj.append(extract_text(soup, "CPF/CNPJ:"))
-            nome_fornecedor.append(extract_text(soup, "Nome Fornecedor:"))
-            Razão_social.append(extract_text(soup, "Razão Social:"))
+            nome_fornecedor.append(extract_text(soup, "Nome do fornecedor:"))
+            Razão_social.append(extract_text(soup, "Razão social :"))
         else:
             print(f"Erro ao acessar o link: {link} - {response.status_code}")
     except Exception as e:
         print(f"Erro ao acessar o link: {link} - {str(e)}")
 
-# Salvando os dados em um arquivo CSV
-with open('contratos.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Código TCE', 'Número Instrumento', 'Ano', 'Situação', 'Objeto', 'Tipo Pessoa', 'Link', 'CPF/CNPJ', 'Nome Fornecedor', 'Razão Social'])
-    for i in range(len(codigo_tce)):
-        writer.writerow([codigo_tce[i], n_instrumento[i], ano[i], situacao[i], objeto[i], tipo_pessoa[i], meus_links[i], cpf_cnpj[i], nome_fornecedor[i], Razão_social[i]])
 
-print("Dados salvos em contratos.csv")
+for i in range(len(codigo_tce)):
+    print(f"Código TCE: {codigo_tce[i]}")
+    print(f"Nº. Instrumento contratual: {n_instrumento[i]}")
+    print(f"Ano: {ano[i]}")
+    print(f"Situação: {situacao[i]}")
+    print(f"Objeto: {objeto[i]}")
+    print(f"Tipo de Pessoa: {tipo_pessoa[i]}")
+    print(f"CPF/CNPJ: {cpf_cnpj[i]}")
+    print(f"Nome Fornecedor: {nome_fornecedor[i]}")
+    print(f"Razão Social: {Razão_social[i]}")
+    print("\n")
+# Salvando os dados em um arquivo CSV
+nomeDoArquivo = input("Digite o nome do csv: ") + '.csv'
+with open(nomeDoArquivo, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Código TCE', 'Número Instrumento', 'Ano', 'Situação', 'Objeto', 'Tipo Pessoa', 'CPF/CNPJ', 'Nome Fornecedor', 'Razão Social'])
+    for i in range(len(codigo_tce)):
+        writer.writerow([codigo_tce[i], n_instrumento[i], ano[i], situacao[i], objeto[i], tipo_pessoa[i], cpf_cnpj[i], nome_fornecedor[i], Razão_social[i]])
+
+print(f"Dados salvos em {nomeDoArquivo}")
